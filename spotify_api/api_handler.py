2e7ac00325757eae6d71646b643a105daf1386b5
RipNasty <det625@gmail.com>
2024-05-21 15:12:26 -0400

    client secret added

--- spotify_api/api_handler.py
+++ spotify_api/api_handler.py
@@ -8,7 +8,7 @@ def access_token():
     tokenRequestBody = {
         'grant_type': 'client_credentials',
         'client_id': '7f2bda3ce59b4026a3907cf750f21ee8',
-        'client_secret': '<your_client_secret>'
+        'client_secret': '506ff237590941348bc7e98b51924c44'
     }
     response = requests.post(url=tokenUri, headers=header, data=tokenRequestBody)
     access_token = response.json()['access_token']

6092ef1126c3f0d54f89e98312793ba0b87f54da
David Thomas <det625@gmail.com>
2025-05-08 14:54:41 -0400

    Update api_handler.py

--- spotify_api/api_handler.py
+++ spotify_api/api_handler.py
@@ -67,7 +67,7 @@ def transform_data(data):
         return None
 
 def save_data_locally(data, filename_prefix="spotify_top_songs"):
-    """Save data to a local JSON file."""
+    """Save data to a JSON file."""
     today = date.today()
     filename = f"{filename_prefix}_{today}.json"
     pd.concat(data).to_json(filename, orient='records')
